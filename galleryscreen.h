39d73f1df5b6921d94066107737cb3795130cc53
rayannnayoubbb <r12122005a@gmail.com>
2025-07-18 21:23:51 +0300

    Update galleryscreen.h

--- galleryscreen.h
+++ galleryscreen.h
@@ -2,6 +2,10 @@
 #define GALLERYSCREEN_H
 
 #include <QWidget>
+#include <QScrollArea>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPushButton>
 
 class GalleryScreen : public QWidget
 {
@@ -10,10 +14,23 @@ public:
     explicit GalleryScreen(QWidget *parent = nullptr);
 
 signals:
-    void backRequested();  // Correct signal name
+    void backRequested();
 
-private slots:
-    void onBackClicked();
+private:
+    void createAgentCards();
+    void addAgentCard(QGridLayout *grid,
+                      const QString &name,
+                      const QString &classType,
+                      const QString &imagePath,
+                      int hp,
+                      int mobility,
+                      int damage,
+                      int range,
+                      int row,
+                      int col);
+
+    QScrollArea *m_scrollArea;
+    QWidget *m_container;
 };
 
 #endif // GALLERYSCREEN_H
